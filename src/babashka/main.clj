c20502be9d266dc5f2423fd7ba9750a130344467
Michiel Borkent <michielborkent@gmail.com>
2020-02-08 16:21:09 +0100

    re-enable pipes

--- src/babashka/main.clj
+++ src/babashka/main.clj
@@ -13,7 +13,7 @@
    [babashka.impl.common :as common]
    [babashka.impl.csv :as csv]
    ;; see https://github.com/oracle/graal/issues/1784
-   #_[babashka.impl.pipe-signal-handler :refer [handle-pipe! pipe-signal-received?]]
+   [babashka.impl.pipe-signal-handler :refer [handle-pipe! pipe-signal-received?]]
    [babashka.impl.repl :as repl]
    [babashka.impl.socket-repl :as socket-repl]
    [babashka.impl.test :as t]
@@ -250,7 +250,7 @@ Everything after that is bound to *command-line-args*."))
    'clojure.java.shell shell-namespace
    'babashka.wait {'wait-for-port wait/wait-for-port
                    'wait-for-path wait/wait-for-path}
-   ;; 'babashka.signal {'pipe-signal-received? pipe-signal-received?}
+   'babashka.signal {'pipe-signal-received? pipe-signal-received?}
    'clojure.java.io io-namespace
    'clojure.core.async async-namespace
    'clojure.core.async.impl.protocols async-protocols-namespace
@@ -281,7 +281,7 @@ Everything after that is bound to *command-line-args*."))
 
 (defn main
   [& args]
-  #_(handle-pipe!)
+  (handle-pipe!)
   #_(binding [*out* *err*]
       (prn "M" (meta (get bindings 'future))))
   (sci/with-bindings {reflection-var false
@@ -295,7 +295,7 @@ Everything after that is bound to *command-line-args*."))
                   :main :uberscript] :as _opts}
           (parse-opts args)
           read-next (fn [*in*]
-                      (if false #_(pipe-signal-received?)
+                      (if (pipe-signal-received?)
                           ::EOF
                           (if stream?
                             (if shell-in (or (read-line) ::EOF)
@@ -406,7 +406,7 @@ Everything after that is bound to *command-line-args*."))
                                                                 edn-out prn)]
                                               (if (coll? res)
                                                 (doseq [l res
-                                                        :while (not false #_(pipe-signal-received?))]
+                                                        :while (not (pipe-signal-received?))]
                                                   (pr-f l))
                                                 (pr-f res))
                                               (prn res)))) 0]]
